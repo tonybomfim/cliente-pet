958b9c9332dea9154937da765b2b251baf8a752b
tonybomfim <ges_tom@hotmail.com>
2023-03-11 22:45:36 -0300

    inicia projeto

---
+++ src/test/java/br/com/petz/Clientepetz/ClientePetzApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petz.Clientepetz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePetzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
